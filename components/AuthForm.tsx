c0d8e1a00de39c45ec65e8ecfda65e054db686c7
NetHunNull <nethunglitch@protonmail.com>
2026-07-14 12:58:12 +0200

    feat: add sign in/up pages

---
+++ components/AuthForm.tsx
@@ -0,0 +1,100 @@
+"use client"
+
+import { createClient } from "@/lib/supabase/client"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
+export default function AuthForm({
+  mode
+}:{
+  mode: "signin" | "signup"
+}) {
+  const supabase = createClient()
+  const router = useRouter()
+
+  const [email, setEmail]=useState("");
+  const [password, setPassword]=useState("");
+  const [message, setMessage]=useState("");
+
+  const submit = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    if (mode === "signup") {
+      const { error } = await supabase.auth.signUp({
+        email, password
+      })
+
+      setMessage(error ? error.message : "Check your email")
+    } 
+    else {
+      const { error } = await supabase.auth.signInWithPassword({
+        email, password
+      });
+
+      setMessage(error ? error.message : "Logged in!")
+    }
+  }
+
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-blue-50">
+      <form
+        onSubmit={submit}
+        className="bg-white p-8 rounded-xl shadow-xl w-96 space-y-4"
+      >
+
+        <h1 className="text-2xl font-bold text-center">
+          { mode === "signin" ? "Sign In" : "Sign Up"}
+        </h1>
+
+        <input
+          className="border p-3 w-full rounded"
+          placeholder="Email"
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input
+          className="border p-3 w-full rounded"
+          placeholder="Password"
+          type="password"
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <p>
+          {mode === "signin" ? (
+            <>
+              If you already have an account:{" "}
+              <button
+                type="button"
+                className="text-blue-600 underline cursor-pointer"
+                onClick={() => router.push("/sign-up")}
+              >
+                Sign Up
+              </button>
+            </>
+          ) : (
+            <>
+              Already have an account?{" "}
+              <button
+                type="button"
+                className="text-blue-600 underline cursor-pointer"
+                onClick={() => router.push("/sign-in")}
+              >
+                Sign In
+              </button>
+            </>
+          )}
+        </p>
+
+        <button
+          className="bg-blue-600 text-white w-full py-3 rounded cursor-pointer hover:bg-blue-800">
+          {mode === "signin" ? "Sign In" : "Create Account"}
+        </button>
+
+        <p className="text-center text-sm">
+          {message}
+        </p>
+
+      </form>
+    </main>
+  );
+}
