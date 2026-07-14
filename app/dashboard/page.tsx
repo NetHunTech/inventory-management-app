04c7328adcf4f2bf5e2f3eafc6d162d20a346230
NetHunNull <nethunglitch@protonmail.com>
2026-07-14 18:50:12 +0200

    chore: finish dashboard page and the sidebar (for now)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,23 @@ export default function Dashboard() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar currenPath="/dashboard"/>
+      <main className="ml-64 p-8">
+        {/* Header */}
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Dashboard
+              </h1>
+              <p className="text-sm text-gray-500">
+                Welcome back! Here is an overview of your inventory.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Key Metric */}
+      </main>
     </div>
   )
 }
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
-import { it } from "node:test"
+import { IoMdSettings } from "react-icons/io";
+import { PiPackage } from "react-icons/pi";
+import { TiPlus } from "react-icons/ti";
+import { MdOutlineInventory } from "react-icons/md";
 
 export default function Sidebar({
   currenPath = "/dashboard"
@@ -9,59 +12,10 @@ export default function Sidebar({
 }) {
 
   const navigation = [
-    {
-      name: "Dashboard", 
-      href: "/dashboard", 
-      icon: (
-        <Image
-          src="/icon.png"
-          width={500}
-          height={300}
-          alt="Dashboard icon"
-          className="rounded-2xl w-15 h-15"
-        />
-      )
-    },
-    {
-      name: "Inventory", 
-      href: "/inventory", 
-      icon: (
-        <Image
-          src="/package.png"
-          width={500}
-          height={300}
-          alt="Inventory icon"
-          className="rounded-2xl w-15 h-15"
-        />
-      )
-    },
-    {
-      name: "Plus", 
-      href: "/add-product", 
-      icon: (
-        <Image
-          src="/icon.png"
-          width={500}
-          height={300}
-          alt="Plus icon"
-          className="rounded-2xl w-15 h-15"
-        />
-      )
-    },
-    {
-      name: "Settings", 
-      href: "/settings", 
-      icon: (
-        <Image
-          src="/settings.png"
-          width={500}
-          height={300}
-          alt="Settings icon"
-          className="rounded-2xl w-15 h-15"
-        />
-      )
-    },
-
+    {name: "Dashboard", href: "/dashboard", icon: <MdOutlineInventory />},
+    {name: "Inventory", href: "/inventory", icon: <PiPackage />},
+    {name: "Plus", href: "/add-product", icon: <TiPlus />},
+    {name: "Settings", href: "/settings", icon: <IoMdSettings />},
   ]
 
   return (
@@ -84,19 +38,28 @@ export default function Sidebar({
           Inventory
         </div>
         {navigation.map((item, key) => {
-
+          const isActive = currenPath === item.href
           return (
             <Link 
               href={item.href} 
               key={key} 
-              className="flex items-center space-x-3"
+              className={
+                `flex items-center space-x-3 py-2 px-3 rounded-lg 
+                ${isActive ? "bg-blue-100 text-gray-800" : "hover:bg-gray-800 text-gray-300"}`
+              }
             >
-              {item.icon}
-              {item.name}
+              <span className="w-5 h-5">{item.icon}</span>
+              <span className="text-sm">{item.name}</span>
             </Link>
           )
         })}
       </nav>
+
+      <div className="absolute bottow-0 left-0 rigt-0 p-6 border-t border-gray-700">
+        <div className="flex item-center justify-between">
+          <span>User here</span>
+        </div>
+      </div>
     </div>
   )
 }
