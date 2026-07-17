d532467cc2040149bf13db051b87d23db52d3a26
NetHunNull <nethunglitch@protonmail.com>
2026-07-17 12:58:54 +0200

    feat: shwo products on 'Inventory' page

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,4 +1,6 @@
 import { createClient } from "@/lib/supabase/server";
+import ProductTable from "@/components/ProductTable";
+import Sidebar from "@/components/SideBar";
 
 export default async function Inventory() {
   const supabase = await createClient()
@@ -17,11 +19,21 @@ export default async function Inventory() {
   }
 
   return (
-    <div>
+    <div className="min-h-screen bg-gray-50">
+      <Sidebar currenPath="/inventory"/>
+      <main className="ml-64 p-8">
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-900 mb-4">
+                Inventory
+              </h1>
 
-      <h1 className="text-2xl font-semibold text-gray-900">
-        Inventory
-      </h1>
+              <ProductTable products={products ?? []}/>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
